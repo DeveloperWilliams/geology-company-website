dbeb769b7bd2865d208d846859678333188218e2
Dev Aj <usikumary@gmail.com>
2024-06-28 02:13:13 +0300

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home/Home'
 
 const App = () => {
   return (
     <>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+      </Routes>
     </>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <>
+     <div className="home">
+        Home
+     </div>
+    </>
+  )
+}
+
+export default Home
