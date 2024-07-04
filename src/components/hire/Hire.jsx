d456ebba6bf5ea5501e498f37200a18b57a063e2
Developer William Achuchi <116683476+DeveloperWilliams@users.noreply.github.com>
2024-07-04 22:46:22 +0300

    Updates

--- src/components/hire/Hire.jsx
+++ src/components/hire/Hire.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import "./hire.css";
 import { Link } from "react-router-dom";
+import { motion, useInView } from "framer-motion";
 
 const Hire = () => {
+  const ref = React.useRef(null);
+  const inView = useInView(ref, { threshold: 0 });
+
   return (
     <>
       <div className="hire">
-        <div className="left">
-          <h3> Lease top-notch equipment from GeoTruth <br className="myBr" />today!</h3>
+        <div className="left" ref={ref}>
+          <h3>
+            Lease top-notch equipment from GeoTruth <br className="myBr" />
+            today!
+          </h3>
         </div>
-        <div className="right">
-         <Link>Lease Equipment Now</Link>
-        </div>
+        <motion.div
+          className="right"
+          initial={{ y: 100, opacity: 0 }}
+          animate={inView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
+          transition={{ duration: 1, delay: 0.4 }}
+        >
+          <Link to="/equipment">Lease Equipment Now</Link>
+        </motion.div>
       </div>
     </>
   );
