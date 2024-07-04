5a31ac361e07e23fea8d57fb742835719d354a98
Developer William Achuchi <116683476+DeveloperWilliams@users.noreply.github.com>
2024-07-04 18:37:49 +0300

    Updates

--- src/components/hire/Hire.jsx
+++ src/components/hire/Hire.jsx
@@ -8,52 +8,7 @@ const Hire = () => {
         <div className="left">
           <h3> Lease top-notch equipment from GeoTruth today!</h3>
         </div>
-        <div className="right">
-          <form>
-            <div>
-              <input type="text" placeholder="First Name" required />
-              <input type="text" placeholder="Surname Name" required />
-            </div>
-            <div>
-              <select name="select" required>
-                <option value="not" disabled selected>
-                  Choose Equipment
-                </option>
-                <option value="Electroseismic">Electroseismic</option>
-                <option value="Electromagnetic">Electromagnetic</option>
-              </select>
-              <div>
-                <label>
-                  Vehicle Included:
-                  <input type="checkbox" name="vehicleIncluded" />
-                </label>
-              </div>
-            </div>
-            <div>
-              <label>
-                Pickup Date:
-                <input type="date" />
-              </label>
-              <label>
-                Return Date:
-                <input type="date" name="returnDate" />
-              </label>
-            </div>
-            <div>
-              <textarea name="msg" placeholder="Leave Comments Here" />
-            </div>
-            <div>
-              <label>
-                Terms and Conditions of Equipment Lease:
-                <input type="checkbox" />
-                Accept
-              </label>
-            </div>
-            <div>
-              <button type="submit">Order Now</button>
-            </div>
-          </form>
-        </div>
+
       </div>
     </>
   );
