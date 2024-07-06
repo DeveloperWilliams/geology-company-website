bad2d0fdfb07e59d3c2947d80f3fa0a7b2a05c36
Developer William Achuchi <116683476+DeveloperWilliams@users.noreply.github.com>
2024-07-06 18:51:13 +0300

    Updates

--- src/components/servicesComponent/esia/Esia.jsx
+++ src/components/servicesComponent/esia/Esia.jsx
@@ -12,32 +12,30 @@ function Esia() {
         <div className="bottom">
           <div className="upper">
             <div className="one">
-              <img src="./image/local.webp" />
+              <img src="./solar1.webp" />
             </div>
             <div className="two">
               <h6>
-                We're East Africa's Leading Solar Installation Specialists,
-                Transforming Solar Potential into Sustainable Energy Solutions.
+                We're East Africa's leading solar installation specialists,
+                transforming solar potential into sustainable energy solutions.
               </h6>
               <p>
-                Our Environmental and Social Impact Assessment (ESIA) and
-                Renewable Energy Services ensure sustainable utilization and
-                management of natural resources, securing valuable assets for
-                diverse industries and advancing renewable energy initiatives.
-                Our expertise encompasses the following spectrum
+                Our Solar Installation Services promote sustainable solar energy
+                management, safeguarding assets across industries and advancing
+                renewable energy initiative
               </p>
               <ul>
                 <li>
-                  <img src="./icon/checked.png" />
+                  <img src="./checked.webp" />
                   ESIA studies for proposed groundwater and development
                   projects.
                 </li>
                 <li>
-                  <img src="./icon/checked.png" />
+                  <img src="./checked.webp" />
                   Energy Audits.
                 </li>
                 <li>
-                  <img src="./icon/checked.png" />
+                  <img src="./checked.webp" />
                   Solar Systems design and installation: Pumping solutions;
                   domestic off-grid and hybrid systems.
                 </li>
