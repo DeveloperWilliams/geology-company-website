abf797c2b752975f1af9ed2f3cf053a0a6b0836a
Developer William Achuchi <116683476+DeveloperWilliams@users.noreply.github.com>
2024-07-12 21:41:38 +0300

    Home Update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import './home.css'
+import React from "react";
+import "./home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="home">
+        <Link to="/ground-water-management">
+          <div className="myservices">
+            <img src="groundwater.webp" alt="Sample Image"></img>
+            <div className="txt">
+              <h1>Groundwater Management</h1>
+            </div>
+          </div>
+        </Link>
+        <Link to="/geology-and-minerals">
+          <div className="myservices">
+            <img src="mine1.webp" alt="Sample Image"></img>
+            <div className="txt">
+              <h1>Geology and Mineral</h1>
+            </div>
+          </div>
+        </Link>
+        <Link to="/solar-system">
+          <div className="myservices">
+            <img src="solar1.webp" alt="Sample Image"></img>
+            <div className="txt">
+               <h1>Solar System</h1>
+            </div>
+          </div>
+        </Link>
+        <Link to="/grid-survey">
+          <div className="myservices">
+            <img src="sv.webp" alt="Sample Image"></img>
+            <div className="txt">
+              <h1>Gridding Survey</h1>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
