611877afdbccf584bf6d5dc2e1cd280e43231633
Developer William Achuchi <116683476+DeveloperWilliams@users.noreply.github.com>
2024-07-12 23:28:53 +0300

    Home Update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,42 +1,72 @@
 import React from "react";
 import "./home.css";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <>
       <div className="home">
         <Link to="/ground-water-management">
-          <div className="myservices">
+          <motion.div className="myservices">
             <img src="groundwater.webp" alt="Sample Image"></img>
             <div className="txt">
-              <h1>Groundwater Management</h1>
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0 }}
+                viewport={{ once: true }} // Ensures the animation only happens once when it comes into view
+              >
+                Groundwater Management
+              </motion.h1>
             </div>
-          </div>
+          </motion.div>
         </Link>
         <Link to="/geology-and-minerals">
-          <div className="myservices">
+          <motion.div className="myservices">
             <img src="mine1.webp" alt="Sample Image"></img>
             <div className="txt">
-              <h1>Geology and Mineral</h1>
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.3 }}
+                viewport={{ once: true }}
+                
+              >
+                Geology and Mineral
+              </motion.h1>
             </div>
-          </div>
+          </motion.div>
         </Link>
         <Link to="/solar-system">
-          <div className="myservices">
+          <motion.div className="myservices">
             <img src="solar1.webp" alt="Sample Image"></img>
             <div className="txt">
-               <h1>Solar System</h1>
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.6 }}
+                viewport={{ once: true }}
+              >
+                Solar System
+              </motion.h1>
             </div>
-          </div>
+          </motion.div>
         </Link>
         <Link to="/grid-survey">
-          <div className="myservices">
+          <motion.div className="myservices">
             <img src="sv.webp" alt="Sample Image"></img>
             <div className="txt">
-              <h1>Gridding Survey</h1>
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.9 }}
+                viewport={{ once: true }}
+              >
+                Gridding Survey
+              </motion.h1>
             </div>
-          </div>
+          </motion.div>
         </Link>
       </div>
     </>
