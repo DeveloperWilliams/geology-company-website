84e69531321121bd63084a82d4b7f767c9c7f61a
DeveloperWilliams <archywilliams2@gmail.com>
2024-07-25 10:04:21 +0300

    Updates

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
   return (
     <div className="loader">
       <div className="outer">
-         <img src="./b.gif" alt="logo" />
+         <img src="./1.gif" alt="logo" />
       </div>
     </div>
   );
